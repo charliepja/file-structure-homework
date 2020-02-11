8e437a21470eb1fda8d0c59df7400b95f405ccd8
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-11 20:05:44 +0000

    adds controllers

--- src/main/java/com/example/codeclan/fileStructure/controller/UserController.java
+++ src/main/java/com/example/codeclan/fileStructure/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.example.codeclan.fileStructure.controller;
 
+import com.example.codeclan.fileStructure.models.User;
+import com.example.codeclan.fileStructure.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(name = "/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(name = "/users/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(name = "/users")
+    public ResponseEntity createNewUser(@RequestBody User newUser) {
+        userRepository.save(newUser);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 }
